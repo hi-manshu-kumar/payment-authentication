01efe276f17f05e61355321cda0d72a8c3877792
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-11-03 00:33:12 +0530

    added paypal integration

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 const handlebar = require('express-handlebars');
-
+const path = require('path');
 const app = express();
+const paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': keys.client_id,
+    'client_secret': keys.client_secret
+  });
 
 //Handlebars Middleware
 app.engine('handlebars', handlebar({ defaultLayout: 'main'}));
@@ -15,13 +22,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 // set static folder
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(__dirname+ '/public'));
 
 // index route
 app.get('/', (req, res) => {
     res.render('index',{
         stripePublishableKey: keys.stripePublishableKey
     }); 
+
 }); 
 
 // app.get('/success', (req, res) => {
@@ -41,6 +49,76 @@ app.post('/charge', (req, res) => {
         currency: 'USD',
         customer:customer.id  
     })).then(charge => res.render('success'));
+});
+
+app.post('/pay', (req, res) => {
+    const create_payment_json = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/success",
+            "cancel_url": "http://localhost:3000/cancel"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "E-Notes",
+                    "sku": "001",
+                    "price": "10.00",
+                    "currency": "USD",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "currency": "USD",
+                "total": "10.00"
+            },
+            "description": "Web Development ebook"
+        }]
+    };
+    
+    
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            for(let i=0; i < payment.links.length; i++){
+                if(payment.links[i].rel === 'approval_url'){
+                    res.redirect(payment.links[i].href);
+                }
+            }
+        }
+    });
+})
+
+app.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+    var execute_payment_json = {
+        "payer_id": payerId,
+        "transactions": [{
+            "amount": {
+                "currency": "USD",
+                "total": "10.00"
+            }
+        }]
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log(JSON.stringify(payment));   
+            res.render("success");
+        }
+    });
+});
+
+app.get('/cancel', (req, res) => {
+    res.send("cancelled");
 });
 
 const port = process.env.PORT || 3000;
