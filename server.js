d01f8231f1acdfdc0ca68b0cab94a7ae9938466f
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-11-09 18:37:41 +0530

    updated codes for paytm

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const handlebar = require('express-handlebars');
 const path = require('path');
 const app = express();
 const paypal = require('paypal-rest-sdk');
+const Paytm = require('paytm-sdk');
+const paytm = new Paytm('keys.merchantkey');
 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
@@ -59,7 +61,7 @@ app.post('/pay', (req, res) => {
         },
         "redirect_urls": {
             "return_url": "http://localhost:3000/success",
-            "cancel_url": "http://localhost:3000/cancel"
+            "cancel_url": "http://localhost:3000/error"
         },
         "transactions": [{
             "item_list": {
@@ -117,8 +119,38 @@ app.get('/success', (req, res) => {
     });
 });
 
+app.post("/paytm", (req, res) => {
+    var params 						= {};
+	params['MID'] 					=keys.merchantkey;//by paytm
+	params['WEBSITE']				= 'WEBSTAGING';//WEBSTAGING
+	params['CHANNEL_ID']			= 'WEB';
+	params['INDUSTRY_TYPE_ID']	= 'Retail';
+	params['ORDER_ID']			= 'TEST_'  + new Date().getTime();
+	params['CUST_ID'] 			= 'Customer001';
+	params['TXN_AMOUNT']		= '720.00';
+	params['CALLBACK_URL']		= 'https://pg-staging.paytm.in/MerchantSite/bankResponse';
+	params['EMAIL']				= 'himanshu.kumar394@gmail.com';
+	params['MOBILE_NO']			= '7011487692';
+
+	checksum_lib.genchecksum(params, keys.merchantkey , function (err, checksum) {
+
+		var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
+		// var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for prod
+		
+		var form_fields = "";
+		for(var x in params){
+			form_fields += "<input type='hidden' name='"+x+"' value='"+params[x]+"' >";
+		}
+		form_fields += "<input type='hidden' name='CHECKSUMHASH' value='"+checksum+"' >";
+
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write('<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="'+txn_url+'" name="f1">'+form_fields+'</form><script type="text/javascript">document.f1.submit();</script></body></html>');
+		res.end();
+	});
+})
+
 app.get('/cancel', (req, res) => {
-    res.send("cancelled");
+    res.render("error");
 });
 
 const port = process.env.PORT || 3000;
